7d0d525f7dce4d1fb2cd14dbdd6fcb17bc23f1dc
Yashawasthi71 <yashawasthi88@gmail.com>
2024-07-11 15:29:00 +0530

    update

--- pattern2.c
+++ pattern2.c
@@ -220,7 +220,7 @@ int main()
     return 0;
 }*/
 
-#include <stdio.h>
+/* #include <stdio.h>
 int main()
 {
     int num;
@@ -242,4 +242,36 @@ int main()
         printf("\n");
     }
         return 0;
+}*/
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; i + j <= num; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = num - 1; i >= 1; i++)
+    {
+        for (int j = 1; j <= (num - i); j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
